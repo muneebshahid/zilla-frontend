1528e0be74c12913784e3c436755f3045586d02d
Muazzam Ali <muazzamalii@hotmail.com>
2019-05-13 20:54:43 +0200

    category box fixed in home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +11,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/assets/js/support.js
@@ -0,0 +1,1 @@
+var job_manager_select2_args = {"width":"100%"};
