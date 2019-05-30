b482335687ffb5b507054da0e588787f9d0dcaae
Muazzam Ali <muazzamalii@hotmail.com>
2019-05-30 20:09:43 +0200

    effects call working

--- frontend/src/app/store/effects/product.ts
+++ frontend/src/app/store/effects/product.ts
@@ -1,29 +1,30 @@
 import { IAppState } from "./../state/app.state";
-import { Store } from "@ngrx/store";
+import { Store, Action } from "@ngrx/store";
 import { Injectable } from "@angular/core";
 import { Effect, ofType, Actions } from "@ngrx/effects";
-import { map } from "rxjs/operators";
+import { map, switchMap, mergeMap } from "rxjs/operators";
 
 import { GetProductDetails, GetProductDetailsSuccess } from "../actions/product";
 
 import { ProductService } from "../../services/product/product.service";
 import { EProductActions } from "../actions/product";
+import { Observable, of } from "rxjs";
 
 @Injectable()
 export class ProductEffects {
   @Effect()
-  getProductDetail$ = this._actions$.pipe(
+  getProductDetail$ = this.actions$.pipe(
     ofType<GetProductDetails>(EProductActions.GetProductDetails),
-    map(action => {
-      console.log("china");
-      console.log(this.productService.getProductDetails(action.payload));
-      this._store.dispatch(new GetProductDetailsSuccess());
+    map(action => action.payload),
+    switchMap(payload => {
+      console.log(payload);
+      return of(new GetProductDetailsSuccess());
     })
   );
 
   constructor(
     private productService: ProductService,
-    private _actions$: Actions,
-    private _store: Store<IAppState>
+    private actions$: Actions,
+    private store: Store<IAppState>
   ) {}
 }
