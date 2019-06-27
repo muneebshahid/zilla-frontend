64548116947292be3449162f456f719cba418ff5
Muazzam Ali <mali2@de.alten.com>
2019-06-27 13:25:22 +0200

    removed unused code in tests

--- frontend/src/app/home-components/home-product-info/home-product-info.component.spec.ts
+++ frontend/src/app/home-components/home-product-info/home-product-info.component.spec.ts
@@ -7,20 +7,6 @@ import { IProduct } from "src/app/models/product";
 describe("HomeProductInfoComponent", () => {
   let component: HomeProductInfoComponent;
   let fixture: ComponentFixture<HomeProductInfoComponent>;
-  // const product: IProduct = {
-  //   product: 0,
-  //   tags: [{ tag: "0", slug: "halal" }],
-  //   images: [{ file: "TEST IMAGE PATH" }],
-  //   latlng: [0, 1],
-  //   title: "DUMMY TITLE",
-  //   slug: "DUMMY SLUG",
-  //   description: "DUMMY DESCRIPTION",
-  //   price: 20,
-  //   available: true,
-  //   product_type: { tag: "DUMMY TAG", slug: "DUMMY SLUG", icon: "DUMMY ICON" },
-  //   owner: 2,
-  //   expensive: 23
-  // };
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -31,8 +17,6 @@ describe("HomeProductInfoComponent", () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(HomeProductInfoComponent);
     component = fixture.componentInstance;
-
-    // component.product = product;
     fixture.detectChanges();
   });
 
