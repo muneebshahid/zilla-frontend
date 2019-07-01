cac9821de1f3f841847f03d3290ac8c611a7be42
Muazzam Ali <muazzamalii@hotmail.com>
2019-07-01 11:19:52 +0200

    products, http tests added

--- frontend/src/app/services/http/http.service.spec.ts
+++ frontend/src/app/services/http/http.service.spec.ts
@@ -2,16 +2,49 @@ import { TestBed } from "@angular/core/testing";
 
 import { HttpService } from "./http.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { IBusiness } from "src/app/models/business";
+import { asyncData, asyncError } from "src/app/testing/async-observable-helpers";
+import { HttpErrorResponse } from "@angular/common/http";
 
 describe("HttpService", () => {
-  beforeEach(() =>
+  let httpClientSpy: { get: jasmine.Spy };
+  let httpService: HttpService;
+  const emptyObs: IBusiness[] = [
+    {
+      user: 0,
+      images: [],
+      business_type: Object.assign({}),
+      opening_timings: [],
+      is_open: false,
+      title: "",
+      slug: "",
+      description: "",
+      website: "",
+      address: "",
+      claimed: false,
+      phone: "",
+      expensive: 0,
+      latlng: []
+    }
+  ];
+
+  beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule]
-    })
-  );
+    });
+    httpClientSpy = jasmine.createSpyObj("HttpClient", ["get"]);
+    httpService = new HttpService(<any>httpClientSpy);
+  });
 
   it("should be created", () => {
-    const service: HttpService = TestBed.get(HttpService);
-    expect(service).toBeTruthy();
+    expect(httpService).toBeTruthy();
+  });
+
+  it("should return expected IBusiness (HttpClient called once)", () => {
+    httpClientSpy.get.and.returnValue(asyncData(emptyObs));
+    httpService
+      .get("DUMMY_URL")
+      .subscribe(result => expect(result).toEqual(emptyObs, "expected result"), fail);
+    expect(httpClientSpy.get.calls.count()).toBe(1, "one call");
   });
 });
