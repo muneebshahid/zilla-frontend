26036dbd5d150f9a4b22b0b27f66635f2ca0ca32
Muazzam Ali <mali2@de.alten.com>
2019-07-02 16:08:25 +0200

    working on ngselect tests

--- frontend/src/app/add-listing-components/general-info/general-info.component.spec.ts
+++ frontend/src/app/add-listing-components/general-info/general-info.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 import { NgSelectModule } from "@ng-select/ng-select";
 import { GeneralInfoComponent } from "./general-info.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { By } from "@angular/platform-browser";
+import { tickAndDetectChanges } from "src/app/testing/general-helpers";
 
 describe("GeneralInfoComponent", () => {
   let component: GeneralInfoComponent;
@@ -22,5 +24,19 @@ describe("GeneralInfoComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+
+  it("should load ng-select successfully", () => {
+    const nativeElement: HTMLElement = fixture.debugElement.query(By.css(".dropdowner"))
+      .nativeElement;
+    tickAndDetectChanges(fixture);
+    const itemsList = fixture.componentInstance.select.itemsList;
+    expect(itemsList.items.length).toBe(7);
+    expect(itemsList.items[0]).toEqual(
+      jasmine.objectContaining({
+        label: "0",
+        value: 0
+      })
+    );
   });
 });
