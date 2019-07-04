bfc695258463f19dae9f41ebfb3f7bb25d8759c4
Muazzam Ali <mali2@de.alten.com>
2019-07-04 12:57:51 +0200

    add remove menu and section tests written for add_listing component

--- frontend/src/app/add-listing-components/add-menu/add-menu.component.spec.ts
+++ frontend/src/app/add-listing-components/add-menu/add-menu.component.spec.ts
@@ -24,16 +24,28 @@ describe("AddMenuComponent", () => {
     expect(component).toBeTruthy();
   });
 
-  it("add new menu link should add a new menu item", () => {
+  it("add new menu link should add a new menu", () => {
     const listingElementDe: DebugElement = fixture.debugElement;
     const addNewMenuDe = listingElementDe.query(By.css(".add-new-menu-price"));
     const menuItemDe = listingElementDe.query(By.css(".menu-prices-section-item-wrapper"));
+    const removeNewMenuDe = listingElementDe.query(By.css(".remove-menu-price"));
 
     expect(menuItemDe.nativeElement.childNodes.length).toEqual(1);
-    jQuery(addNewMenuDe.nativeElement).trigger("click");
+    addNewMenuDe.nativeElement.click();
     expect(menuItemDe.nativeElement.childNodes.length).toEqual(2);
+    removeNewMenuDe.nativeElement.click();
+    expect(menuItemDe.nativeElement.childNodes.length).toEqual(1);
   });
-  it("add new section link should add a new menu item", () => {});
-  it("remove new menu link should add a new menu item", () => {});
-  it("remove new section link should add a new menu item", () => {});
+  it("add new section link should add a new section", () => {
+    const listingElementDe: DebugElement = fixture.debugElement;
+    const addNewSectionDe = listingElementDe.query(By.css(".add-new-section-menu-price"));
+    const removeNewSectionDe = listingElementDe.query(By.css(".remove-section-menu-price"));
+    const menuSectionItemDe = listingElementDe.query(By.css(".menu-prices-field-wrapper"));
+
+    expect(menuSectionItemDe.nativeElement.childNodes.length).toEqual(1);
+    addNewSectionDe.nativeElement.click();
+    expect(menuSectionItemDe.nativeElement.childNodes.length).toEqual(2);
+    removeNewSectionDe.nativeElement.click();
+    expect(menuSectionItemDe.nativeElement.childNodes.length).toEqual(1);
+  });
 });
--- frontend/src/assets/js/listing.js
+++ frontend/src/assets/js/listing.js
@@ -1299,7 +1299,7 @@
     submitForm: function() {
       var self = this;
       // section
-      $(".add-new-section-menu-price").on("click", function(e) {
+      $("body").on("click", ".add-new-section-menu-price", function(e) {
         e.preventDefault();
         var length = $(".menu-prices-field-wrapper .menu-prices-section-item").length;
         var html = $(".menu-prices-field-wrapper .menu-prices-section-item")
@@ -1321,7 +1321,7 @@
         $(".menu-prices-field-wrapper").append(html);
       });
 
-      $(".remove-section-menu-price").on("click", function(e) {
+      $("body").on("click", ".remove-section-menu-price", function(e) {
         e.preventDefault();
         var index = $(".menu-prices-field-wrapper .menu-prices-section-item")
           .last()
@@ -1337,21 +1337,12 @@
       $("body").on("click", ".add-new-menu-price", function(e) {
         e.preventDefault();
         var parent = $(this).parent();
-        console.log("parent1");
-        console.log(parent.find(".menu-prices-section-item-wrapper"));
-        // console.log(
-        //   $(".menu-prices-item", parent)
-        //     .eq(0)
-        //     .clone(true)
-        // );
 
         parent.find(".menu-prices-section-item-wrapper").append(
           $(".menu-prices-item", parent)
             .eq(0)
             .clone(true)
         );
-        console.log("parent2");
-        console.log(parent.find(".menu-prices-section-item-wrapper"));
       });
       $("body").on("click", ".remove-menu-price", function(e) {
         e.preventDefault();
