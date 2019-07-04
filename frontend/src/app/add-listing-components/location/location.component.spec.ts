642b6e75d3744cc58bfe4ef81b86aeefaf95a776
Muazzam Ali <mali2@de.alten.com>
2019-07-04 14:41:14 +0200

    labels testing for location component added

--- frontend/src/app/add-listing-components/location/location.component.spec.ts
+++ frontend/src/app/add-listing-components/location/location.component.spec.ts
@@ -1,8 +1,9 @@
-import { Component, Input, NgZone } from "@angular/core";
+import { Component, Input, NgZone, DebugElement } from "@angular/core";
 import { async, ComponentFixture, TestBed, inject } from "@angular/core/testing";
 import { LocationComponent } from "./location.component";
 import { AgmCoreModule, GoogleMapsAPIWrapper, MapsAPILoader } from "@agm/core";
 import { GeoLocationService } from "src/app/services/geo-location/geo-location.service";
+import { By } from "@angular/platform-browser";
 
 @Component({ selector: "app-map", template: "" })
 class MapComponent {
@@ -34,5 +35,18 @@ describe("LocationComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should ensure that the labels are named correctly", () => {
+    const listingElementDe: DebugElement = fixture.debugElement;
+
+    const locationDe = listingElementDe.query(By.css(".location"));
+    const websiteDe = listingElementDe.query(By.css(".website"));
+    const phoneDe = listingElementDe.query(By.css(".phone"));
+    const emailDe = listingElementDe.query(By.css(".email"));
+
+    expect(locationDe.nativeElement.textContent).toEqual("Location");
+    expect(websiteDe.nativeElement.textContent).toEqual("Website");
+    expect(phoneDe.nativeElement.textContent).toEqual("Phone");
+    expect(emailDe.nativeElement.textContent).toEqual("Email");
   });
 });
