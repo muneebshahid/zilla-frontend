abc1bea220dcaee44f4511faa0a2ae289717b9fc
Muazzam Ali <muazzamalii@hotmail.com>
2019-07-25 19:24:37 +0200

    loading sign implemented

--- frontend/src/app/general-components/loading/loading.component.ts
+++ frontend/src/app/general-components/loading/loading.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit, Input } from "@angular/core";
   styleUrls: ["./loading.component.css"]
 })
 export class LoadingComponent implements OnInit {
-  @Input() transparency = "";
-
   constructor() {}
 
   ngOnInit() {}
