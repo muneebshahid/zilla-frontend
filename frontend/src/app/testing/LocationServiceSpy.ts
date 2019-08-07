e0920da297871b73451a4701228215b27cab112e
Muazzam Ali <mali2@de.alten.com>
2019-08-07 16:05:04 +0200

    removed fakeStub for locatioNService

--- frontend/src/app/testing/LocationServiceSpy.ts
+++
@@ -1,8 +0,0 @@
-import { asyncData } from "./async-observable-helpers";
-
-export class LocationServiceSpy {
-  /* emit cloned test hero */
-  setDetailLocation = jasmine
-    .createSpy("setDetailLocation")
-    .and.callFake(() => asyncData(Object.assign({})));
-}
