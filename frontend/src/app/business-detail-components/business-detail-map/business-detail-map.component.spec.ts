429a25ddb5b5b190706028af093d7954a8e8af4c
Muazzam Ali <muazzamalii@hotmail.com>
2019-08-10 18:48:37 +0200

    detail map business component

--- frontend/src/app/business-detail-components/business-detail-map/business-detail-map.component.spec.ts
+++ frontend/src/app/business-detail-components/business-detail-map/business-detail-map.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-import { Component, Input } from "@angular/core";
 
 import { BusinessDetailMapComponent } from "./business-detail-map.component";
 import { MapComponent } from "src/app/testing/dummy_components";
@@ -7,8 +6,10 @@ import { MapComponent } from "src/app/testing/dummy_components";
 describe("BusinessDetailMapComponent", () => {
   let component: BusinessDetailMapComponent;
   let fixture: ComponentFixture<BusinessDetailMapComponent>;
-
+  let mapComponentSpy;
   beforeEach(async(() => {
+    mapComponentSpy = jasmine.createSpyObj("MapComponent", ["setFocusLocation"]);
+
     TestBed.configureTestingModule({
       declarations: [BusinessDetailMapComponent, MapComponent]
     }).compileComponents();
@@ -22,5 +23,17 @@ describe("BusinessDetailMapComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should call setFocusLocation for mapComponent", () => {
+    component.latlon = [1, 2];
+    component.mapComponent = mapComponentSpy;
+    component.ngOnChanges({});
+    expect(mapComponentSpy.setFocusLocation).toHaveBeenCalled();
+  });
+  it("should not call setFocusLocation for mapComponent", () => {
+    component.latlon = null;
+    component.mapComponent = mapComponentSpy;
+    component.ngOnChanges({});
+    expect(mapComponentSpy.setFocusLocation).not.toHaveBeenCalled();
   });
 });
