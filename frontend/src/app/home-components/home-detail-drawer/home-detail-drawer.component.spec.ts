8eb19053aa06d2e4dff26fdaccc3aa3bdcd8ee69
Muazzam Ali <mali2@de.alten.com>
2019-08-14 14:55:27 +0200

    home-detail-drawer component

--- frontend/src/app/home-components/home-detail-drawer/home-detail-drawer.component.spec.ts
+++ frontend/src/app/home-components/home-detail-drawer/home-detail-drawer.component.spec.ts
@@ -3,15 +3,28 @@ import {
   DetailHeaderComponent,
   BusinessDetailContainerComponent
 } from "./../../testing/dummy_components";
-import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from "@angular/core/testing";
 
 import { HomeDetailDrawerComponent } from "./home-detail-drawer.component";
-import { StoreModule } from "@ngrx/store";
+import { StoreModule, Store } from "@ngrx/store";
 import { appReducers } from "src/app/store/reducers/app.reducer";
+import { ElementRef } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { IAppState } from "src/app/store/state/app.state";
+import { UpdateCloseDetailDrawer } from "src/app/store/actions/general";
+
+declare var jQuery: any;
+
+export class MockElementRef extends ElementRef {
+  constructor() {
+    super(null);
+  }
+}
 
 describe("HomeDetailDrawerComponent", () => {
   let component: HomeDetailDrawerComponent;
   let fixture: ComponentFixture<HomeDetailDrawerComponent>;
+  let store: Store<IAppState>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,8 +34,12 @@ describe("HomeDetailDrawerComponent", () => {
         DetailHeaderComponent,
         BusinessDetailContainerComponent
       ],
+      providers: [{ provide: ElementRef, useValue: MockElementRef }],
       imports: [StoreModule.forRoot(appReducers)]
     }).compileComponents();
+
+    store = TestBed.get(Store);
+    spyOn(store, "dispatch").and.callThrough();
   }));
 
   beforeEach(() => {
@@ -33,5 +50,33 @@ describe("HomeDetailDrawerComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy();
+  });
+  it("should not close drawer and update lastState since magnific popup was open", () => {
+    jQuery("body").addClass("dummy-class");
+    let detailContainerElement = fixture.debugElement.queryAll(By.css(".listings-detail-wrapper"));
+    let detailContainerElementNative: HTMLElement = detailContainerElement[0].nativeElement;
+    detailContainerElementNative.click();
+    expect(component.lastState).toBe("pop up was not open");
+  });
+  it("should close popup but not drawer and update lastState", () => {
+    jQuery("body").addClass("mfp-zoom-out-cur");
+    let detailContainerElement = fixture.debugElement.queryAll(By.css(".listings-detail-wrapper"));
+    let detailContainerElementNative: HTMLElement = detailContainerElement[0].nativeElement;
+    detailContainerElementNative.click();
+    expect(component.lastState).toBe("popup was open");
+  });
+  it("should close drawer and update lastState", fakeAsync(() => {
+    jQuery("body").addClass("mfp-zoom-out-cur");
+    jQuery("body").click();
+    tick(1000);
+    let closeDrawerAction = new UpdateCloseDetailDrawer(true);
+    expect(store.dispatch).toHaveBeenCalledWith(closeDrawerAction);
+  }));
+  it("should set the lastState as the popup was not open", () => {
+    component.lastState = "popup was open";
+    jQuery("body").addClass("mfp-zoom-out-cur");
+    jQuery("body").click();
+
+    expect(component.lastState).toBe("pop up was not open");
   });
 });
